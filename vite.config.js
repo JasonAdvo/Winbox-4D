a1ce697cf70959cf3f92110dd9a5b6f95ea14720
root <root@ubuntu-4d.winboxvip.vip>
2024-08-19 07:57:41 +0000

    update build

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,30 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+build: {
+			target: "modules",
+			outDir: "../4d.winboxvip.vip",
+			assetsDir: "assets",
+			assetsInlineLimit: 4096,
+			cssCodeSplit: true,
+			sourcemap: false,
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes('node_modules')) {
+							return id.toString().split('node_modules/')[1].split('/')[0].toString();
+						}
+					}
+				}
+			},
+			commonjsOptions: {},
+			manifest: false,
+			minify: "terser", //terser 构建后文件体积更小
+			terserOptions: {},
+			write: true,
+			emptyOutDir: true,
+			brotliSize: true,
+			chunkSizeWarningLimit: 500
+		},
 })
